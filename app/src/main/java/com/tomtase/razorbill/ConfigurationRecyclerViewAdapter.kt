c46485b40e7c82ea548802ed375fbda02458ad1d
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2019-05-26 15:28:36 +0300

    Started to implement complication config

--- app/src/main/java/com/tomtase/razorbill/ConfigurationRecyclerViewAdapter.kt
+++ app/src/main/java/com/tomtase/razorbill/ConfigurationRecyclerViewAdapter.kt
@@ -2,6 +2,11 @@ package com.tomtase.razorbill
 
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
+import android.R
+import android.view.LayoutInflater
+
+
+const val TYPE_PREVIEW_AND_COMPLICATIONS_CONFIG = 0
 
 class ConfigurationRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -12,15 +17,56 @@ class ConfigurationRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewH
         LEFT
     }
 
+    private val settings: ArrayList<ConfigItemType>? = null
+    private val previewViewHolder: PreviewViewHolder? = null
+
     override fun getItemCount(): Int {
-        TODO("not implemented")
+        if (settings != null) {
+            return settings.count()
+        }
+        return 0
     }
 
-    override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
-        TODO("not implemented")
+    override fun getItemViewType(position: Int): Int {
+        val configItemType = settings.get(position)
+        return configItemType.getConfigType()
     }
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
-        TODO("not implemented")
+
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+        val configItemType = settings.get(position)
+
+        when (viewHolder.itemViewType) {
+            TYPE_PREVIEW_AND_COMPLICATIONS_CONFIG -> {
+                val previewAndComplicationsViewHolder = viewHolder as PreviewViewHolder
+
+                val previewAndComplicationsConfigItem = configItemType as ConfigItem
+
+                val defaultComplicationResourceId = previewAndComplicationsConfigItem.getDefaultComplicationResourceId()
+                previewAndComplicationsViewHolder.setDefaultComplicationDrawable(
+                    defaultComplicationResourceId
+                )
+
+                previewAndComplicationsViewHolder.initializesColorsAndComplications()
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        when (viewType) {
+            TYPE_PREVIEW_AND_COMPLICATIONS_CONFIG -> {
+                previewViewHolder = PreviewViewHolder(
+                    LayoutInflater.from(parent.context)
+                        .inflate(
+                            R.layout.config_list_preview,
+                            parent,
+                            false
+                        )
+                )
+                return previewViewHolder
+            }
+        }
+        return null;
     }
 }
